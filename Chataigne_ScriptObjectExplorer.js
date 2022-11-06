5d17610ffd033b092082f3568dc928f09a32a8ec
eLandon <landon.etienne@gmail.com>
2022-11-06 11:18:13 +0100

    Initial commit

---
+++ Chataigne_ScriptObjectExplorer.js
@@ -0,0 +1,53 @@
+
+
+
+/////////////////////////   INIT   ///////////////////////////////
+
+function init(){
+    local.scripts.chataigne_ScriptObjectExplorer.enableLog.set(true);
+}
+
+
+function moduleParameterChanged(param){
+
+    if(param.is(local.parameters.print)){
+        getObjectString();
+    }
+    if(param.is(local.parameters.object)){
+        getObjectString();
+    }
+}
+
+function getObjectString(){
+    var splitAdress = local.parameters.object.get().split(".");
+        var obj = root;
+        var lost = false;
+        for (var i=1; i<splitAdress.length; i++){
+            obj = obj.getChild(splitAdress[i]);
+            if (obj==undefined){lost = true;}
+        }
+        if(!lost){getMethodsProperties(obj);}
+        else{script.log("script adress not recognized");}
+        
+}
+
+function getMethodsProperties(myObject){
+    script.log("OBJECT : "+local.parameters.object.get());
+	var log = util.getObjectProperties(myObject, true, true);
+	script.log("Properties :");
+	for (var i = 0 ; i<log.length; i++){
+		script.log(".      "+log[i]);
+	}
+    script.log("\n");
+	log = util.getObjectMethods(myObject);
+	script.log("Methods :");
+	for (var i = 0 ; i<log.length; i++){
+		script.log(".      "+log[i]);
+	}
+    script.log("\n");
+}
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Chataigne script object explorer
+
+This module allows to get all methods and properties of any object in chataigne by providing it's script adress.
+
+Get the script adress of desired object by right-clicking and "copy script adress". Paste the adress in the "object" string and look at log.
+If needed you may re-print using "print" trigger
